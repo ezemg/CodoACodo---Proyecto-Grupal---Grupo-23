7fb42bc18c0f47d8f1c07796b4c38e3b163c2464
amgamb <ana.m.boadas@exxonmobil.com>
2023-10-23 21:02:14 -0300

    Validacion lista! *.*

--- src/public/js/validacionform.js
+++ src/public/js/validacionform.js
@@ -1,27 +1,40 @@
+
 document
   .getElementById("formulario")
   .addEventListener("submit", function (event) {
     event.preventDefault(); // prevent event to get into default //
-
+  
     //Llama a los valores del formulario//
     const name = document.getElementById("nombre").value;
-    const name = document.getElementById("mail").value;
-    const name = document.getElementById("telefono").value;
-    const name = document.getElementById("mensaje").value;
+    const mail = document.getElementById("mail").value;
+    const telefono = document.getElementById("telefono").value;
+    const message = document.getElementById("message").value;
+
 
     //Lógica de la validación//
-    if (name === "" || mail === "" || telefono === "" || mensaje === "") {
+    if (name === "" || mail === "" || telefono === "" || message === "") {
       alert("Todos los campos son requeridos");
-    } else if (!isvalidemail(email)) {
+    } 
+      else if (!isValidemail(mail)) {
       alert("El mail es incorrecto :(");
-    } else {
-      alert("Gracias por tu consulta, en breve nos contactaremos");
-    }
-  });
+      }
+      else if (!isValidPhone(telefono)) {
+        alert ("El teléfono debe contener al menos 10 números :/");
+      }
+     else {
+      alert("Gracias por tu consulta, en breve nos contactaremos")};
+    
 
 // Funcion de validación//
-function isvalidemail(email) {
+function isValidemail(mail) {
   const emailregex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  return emailregex.test(email);
+  return emailregex.test(mail);
   alert("El mail es incorrecto :(");
 }
+function isValidPhone(telefono) {
+  const phoneRegex = /^\d{10,14}$/;
+  return phoneRegex.test(telefono)
+  alert ("El teléfono debe contener al menos 10 números :/");
+}
+});
+
